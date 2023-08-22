11e3bfd39c49daf45b4dd941ce7144d0d4102c36
Quinn Davis <qdavis6106@stu.d214.org>
2023-08-22 10:56:47 -0500

    Initial Commit

---
+++ ContentView.swift
@@ -0,0 +1,18 @@
+//
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image("dog")
+            Text("Hello, world!")
+        }
+    }
+}
+
+
+//struct ContentViewForcePreview: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
