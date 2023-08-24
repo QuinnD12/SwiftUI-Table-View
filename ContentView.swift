841a0c00200470392f87ef68a1c2bf026d0c5d11
Quinn Davis <qdavis6106@stu.d214.org>
2023-08-24 11:05:38 -0500

    Lot's of errors doesn't work

--- ContentView.swift
+++ ContentView.swift
@@ -1,12 +1,29 @@
-//
 import SwiftUI
+
+struct Class: Identifiable {
+    let id = UUID()
+    let className: String
+    let period: Int
+}
+
+let schedule = [
+    Class(className: "", period: 1),
+    Class(className: "", period: 2),
+    Class(className: "", period: 3),
+    Class(className: "", period: 4),
+    Class(className: "", period: 5),
+    Class(className: "", period: 6),
+    Class(className: "", period: 7),
+    Class(className:"", period: 8)
+]
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image("dog")
-            Text("Hello, Bryan!")
-                .bold()
+        Table(schedule) {
+            TableColumn("Class", value: \.className)
+            TableColumn("Period") { classInstance in
+                Text(String(classInstance.period))
+            }
         }
     }
 }
