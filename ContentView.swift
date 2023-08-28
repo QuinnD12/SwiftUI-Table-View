47bbb102acb5151dc0b2b2a5e3f467faaf9a114f
Quinn Davis <qdavis6106@stu.d214.org>
2023-08-28 10:33:53 -0500

    Works now doesn't look great

--- ContentView.swift
+++ ContentView.swift
@@ -7,42 +7,42 @@ struct Class: Identifiable {
 }
 
 let schedule = [
-    Class(className: "", period: 1),
-    Class(className: "", period: 2),
-    Class(className: "", period: 3),
-    Class(className: "", period: 4),
-    Class(className: "", period: 5),
-    Class(className: "", period: 6),
-    Class(className: "", period: 7),
-    Class(className:"", period: 8)
+    Class(className: "Math", period: 1),
+    Class(className: "Reading", period: 2),
+    Class(className: "Writing", period: 3),
+    Class(className: "Chemistry", period: 4),
+    Class(className: "Biology", period: 5),
+    Class(className: "Gym", period: 6),
+    Class(className: "Study Hall", period: 7),
+    Class(className:"History", period: 8)
 ]
 
 struct ContentView: View {
     var body: some View {
-        if #available(iOS 16.0, *) {
-            Text("Runnig 16+")
-            
-            Table(schedule) {
-                TableColumn("Class", value: \.className)
-                TableColumn("Period") { classInstance in
-                    Text(String(classInstance.period))
-                }
-            }
-        } else {
-            List(schedule) { classInstance in
-                HStack {
-                    Text(classInstance.className)
-                    Spacer()
-                    Text(String(classInstance.period))
-                }
+        HStack {
+            Text("Class")
+                .bold()
+                .padding()
+            Spacer()
+            Text("Period")
+                .bold()
+                .padding()
+        }
+        
+        List(schedule) { classInstance in
+            HStack {
+                Text(classInstance.className)
+                Spacer()
+                Text(String(classInstance.period))
             }
         }
     }
 }
 
 
-struct ContentViewForcePreview: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
+
+//struct ContentViewForcePreview: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
