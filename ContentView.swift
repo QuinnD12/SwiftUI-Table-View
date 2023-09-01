d0c4cf421a2b6101b7604d487ac14a4aeb9b8153
Quinn Davis <qdavis6106@stu.d214.org>
2023-09-01 11:02:40 -0500

    Teams and scrollable

--- ContentView.swift
+++ ContentView.swift
@@ -16,21 +16,22 @@ struct Player: Identifiable {
     let id = UUID()
     let name: String
     let position: Position
+    let team: String
 }
 
 
 let schedule = [
-    Player(name: "Trevor Lawrence", position: .qb),
-    Player(name: "Kenneth Walker III", position: .rb),
-    Player(name: "Jamaal Williams", position: .rb),
-    Player(name: "Garrett Wilson", position: .wr),
-    Player(name: "Jaylen Waddle", position: .wr),
-    Player(name: "Cole Kmet", position: .te),
-    Player(name: "Nico Collins", position: .flex),
-    Player(name: "Steelers D/ST", position: .dst),
-    Player(name: "Dustin Hopkins", position: .k),
-    Player(name: "AJ Cole", position: .p),
-    Player(name: "Lions HC", position: .hc)
+    Player(name: "Trevor Lawrence", position: .qb, team: "Jacksonville"),
+    Player(name: "Kenneth Walker III", position: .rb, team: "Seattle"),
+    Player(name: "Jamaal Williams", position: .rb, team: "New Orleans"),
+    Player(name: "Garrett Wilson", position: .wr, team: "New York(Jets)"),
+    Player(name: "Jaylen Waddle", position: .wr, team: "Miami"),
+    Player(name: "Cole Kmet", position: .te, team: "Chicago"),
+    Player(name: "Nico Collins", position: .flex, team: "Houston"),
+    Player(name: "Steelers D/ST", position: .dst, team: "Pittsburgh"),
+    Player(name: "Dustin Hopkins", position: .k, team: "Cleveland"),
+    Player(name: "AJ Cole", position: .p, team: "Las Vegas"),
+    Player(name: "Lions HC", position: .hc, team: "Detriot")
 ]
 
 struct ContentView: View {
@@ -44,13 +45,22 @@ struct ContentView: View {
                 Text("Position")
                     .font(.title)
                     .bold()
+                Spacer()
+                Text("Team")
+                    .font(.title)
+                    .bold()
             }
             
             ForEach(schedule) { player in
-                HStack {
-                    Text(player.name)
-                    Spacer()
-                    Text(player.position.rawValue)
+                ScrollView(.horizontal) {
+                    HStack {
+                        Text(player.name)
+                        Spacer()
+                        Text(player.position.rawValue)
+                        Spacer()
+                        Text(player.team)
+                    }
+                    .frame(width: 325, height: 25)
                 }
             }
         }
